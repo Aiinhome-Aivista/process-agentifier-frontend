7011520279239942f8c5bd1a9d4a5f31b796af93
Pinki Paul <pp3785828@gmail.com>
2026-03-19 20:22:07 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://157.173.221.226:3006/api',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/',
+    plugins: [react()],
+  };
+});
