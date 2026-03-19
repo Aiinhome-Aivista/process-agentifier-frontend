458f5579bd14c2d28796775245911fcf3108e7f8
Pinki Paul <pp3785828@gmail.com>
2026-03-19 20:16:48 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://157.173.221.226:3006/api',
         changeOrigin: true,
       }
     }
